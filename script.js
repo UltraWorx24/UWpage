b1481a4d507a237bf6bc7bbe4ed38ebb3db6b738
UltraWorx24 <Michael@Spider-worx.com>
2025-03-25 09:30:58 +0000

    js.script

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const heroImage = document.querySelector('.hero-image');
-    if (heroImage) {
-        heroImage.style.display = 'block';
-    }
 
     const header = document.querySelector('header');
     window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
+        if (window.scrollY > 10) {
             header.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
